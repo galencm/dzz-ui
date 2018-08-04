a5a96f5a75e175ffcbe210e9685f8b7d6af421f1
Galen Curwen-McAdams <galencm@fastmail.com>
2018-08-04 05:55:36 +0000

    Problem: no minimal kivy app
    Solution: create minimal app

--- dzz_ui/dzz_ui.py
+++ dzz_ui/dzz_ui.py
@@ -1,11 +1,37 @@
 import argparse
+import atexit
 import redis
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
 from ma_cli import data_models
 from lings import ruling, pipeling
 
 r_ip, r_port = data_models.service_connection()
 binary_r = redis.StrictRedis(host=r_ip, port=r_port)
 redis_conn = redis.StrictRedis(host=r_ip, port=r_port, decode_responses=True)
+
+class DzzApp(App):
+
+    def __init__(self, *args, **kwargs):
+        # store kwargs to passthrough
+        self.kwargs = kwargs
+        if kwargs["db_host"] and kwargs["db_port"]:
+            global binary_r
+            global redis_conn
+            db_settings = {"host" :  kwargs["db_host"], "port" : kwargs["db_port"]}
+            binary_r = redis.StrictRedis(**db_settings)
+            redis_conn = redis.StrictRedis(**db_settings, decode_responses=True)
+
+        super(DzzApp, self).__init__()
+
+    def save_session(self):
+        pass
+
+    def build(self):
+        root = BoxLayout()
+        return root
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,3 +41,7 @@ def main():
 
     if bool(args.db_host) != bool(args.db_port):
         parser.error("--db-host and --db-port values are both required")
+
+    app = DzzApp(**vars(args))
+    atexit.register(app.save_session)
+    app.run()
