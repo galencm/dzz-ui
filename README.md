cf87e9dafaa30cb2a215488ea108b921f1731ecb
Galen Curwen-McAdams <galencm@fastmail.com>
2018-08-04 05:32:04 +0000

    Problem: dss-ui feels needlessly complex
    Solution: create dzz-ui to be a more coherent
    interface, learning from dss and simplifying
    as needed.

---
+++ README.md
@@ -0,0 +1,37 @@
+# dzz-ui
+
+_dss-ui v2_
+
+## Installation
+
+Pip:
+
+```
+pip3 install git+https://github.com/galencm/dzz-ui --user --process-dependency-links
+```
+
+Develop while using pip:
+
+```
+git clone https://github.com/galencm/dzz-ui
+cd dzz-ui/
+pip3 install --editable ./ --user --process-dependency-links
+```
+
+## Usage
+
+```
+dzz-ui --size=1500x800
+```
+
+## Contributing
+This project uses the C4 process 
+
+[https://rfc.zeromq.org/spec:42/C4/](https://rfc.zeromq.org/spec:42/C4/
+)
+
+## License
+Mozilla Public License, v. 2.0
+
+[http://mozilla.org/MPL/2.0/](http://mozilla.org/MPL/2.0/)
+
---
+++ dzz_ui/dzz_ui.py
@@ -0,0 +1,17 @@
+import argparse
+import redis
+from ma_cli import data_models
+from lings import ruling, pipeling
+
+r_ip, r_port = data_models.service_connection()
+binary_r = redis.StrictRedis(host=r_ip, port=r_port)
+redis_conn = redis.StrictRedis(host=r_ip, port=r_port, decode_responses=True)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--db-host",  help="db host ip, requires use of --db-port")
+    parser.add_argument("--db-port", type=int, help="db port, requires use of --db-host")
+    args = parser.parse_args()
+
+    if bool(args.db_host) != bool(args.db_port):
+        parser.error("--db-host and --db-port values are both required")
